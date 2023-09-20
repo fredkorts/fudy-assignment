fb2e928d818121766c568903439512232baa6bb2
fredkorts <fredkorts@gmail.com>
2023-09-20 19:45:55 +0300

    Add service for restaurants.

---
+++ src/app/services/restaurant.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';  // <-- Import HttpParams
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+
+  private apiUrl = 'https://dev-api.fudy.eu/restaurants';
+
+  constructor(private http: HttpClient) { }
+
+  getRestaurants(page: number, take: number, status?: 'ACTIVE' | 'INACTIVE'): Observable<any> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('take', take.toString());
+
+    if (status) {
+      params = params.set('status', status);
+    }
+
+    return this.http.get(this.apiUrl, { params });
+  }
+}
